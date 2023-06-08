fab716d10017ce4319f2e9715aee9324746044f8
WangXuan95 <629708558@qq.com>
2023-06-08 18:40:43 +0800

    change to Verilog2001

--- FPGA/uart_tx.v
+++ FPGA/uart_tx.v
@@ -16,8 +16,8 @@ module uart_tx #(
     parameter  STOP_BITS                 = 2,            // can be 1, 2, 3, 4, ...
     // AXI stream data width
     parameter  BYTE_WIDTH                = 1,            // can be 1, 2, 3, 4, ...
-    // TX fifo
-    parameter  FIFO_EA                   = 0,            // 0:no TX fifo   3:fifo_depth=8   4:fifo_depth=16  ...  9:fifo_depth=512   10:fifo_depth=1024   11:fifo_depth=2048  ...
+    // TX fifo depth
+    parameter  FIFO_EA                   = 0,            // 0:no fifo   1,2:depth=4   3:depth=8   4:depth=16  ...  10:depth=1024   11:depth=2048  ...
     // do you want to send extra byte after each AXI-stream transfer or packet?
     parameter  EXTRA_BYTE_AFTER_TRANSFER = "",           // specify a extra byte to send after each AXI-stream transfer. when ="", do not send this extra byte
     parameter  EXTRA_BYTE_AFTER_PACKET   = ""            // specify a extra byte to send after each AXI-stream packet  . when ="", do not send this extra byte
@@ -45,19 +45,17 @@ reg  [8*BYTE_WIDTH-1:0] f_tdata;
 reg  [  BYTE_WIDTH-1:0] f_tkeep;
 reg                     f_tlast;
 
-generate if (FIFO_EA < 3) begin          // no TX fifo
+generate if (FIFO_EA <= 0) begin          // no TX fifo
 
-    assign i_tready = f_tready;
-    always @ (*) begin
-        f_tvalid = i_tvalid;
-        f_tdata  = i_tdata;
-        f_tkeep  = i_tkeep;
-        f_tlast  = i_tlast;
-    end
+    assign       i_tready = f_tready;
+    always @ (*) f_tvalid = i_tvalid;
+    always @ (*) f_tdata  = i_tdata;
+    always @ (*) f_tkeep  = i_tkeep;
+    always @ (*) f_tlast  = i_tlast;
 
-end else begin                           // TX fifo
+end else begin                            // TX fifo
 
-    localparam        EA     = FIFO_EA;
+    localparam        EA     = (FIFO_EA<=2) ? 2 : FIFO_EA;
     localparam        DW     = ( 1 + BYTE_WIDTH + 8*BYTE_WIDTH );     // 1-bit tlast, (BYTE_WIDTH)-bit tkeep, (8*BYTE_WIDTH)-bit tdata
     
     reg  [DW-1:0] buffer [ ((1<<EA)-1) : 0 ];
@@ -113,130 +111,31 @@ end endgenerate
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
 localparam      BAUD_CYCLES              = ( (CLK_FREQ*10*2 + BAUD_RATE) / (BAUD_RATE*2) ) / 10 ;
 localparam      BAUD_CYCLES_FRAC         = ( (CLK_FREQ*10*2 + BAUD_RATE) / (BAUD_RATE*2) ) % 10 ;
-
-localparam real IDEAL_BAUD_CYCLES        = (1.0*CLK_FREQ) / (1.0*BAUD_RATE);
-localparam real ACTUAL_BAUD_CYCLES       = (10.0*BAUD_CYCLES + BAUD_CYCLES_FRAC) / 10.0;
-localparam real ACTUAL_BAUD_RATE         = (1.0*CLK_FREQ) / ACTUAL_BAUD_CYCLES;
-localparam real BAUD_RATE_ERROR          = (ACTUAL_BAUD_RATE > 1.0*BAUD_RATE) ? (ACTUAL_BAUD_RATE - 1.0*BAUD_RATE) : (1.0*BAUD_RATE - ACTUAL_BAUD_RATE);
-localparam real BAUD_RATE_RELATIVE_ERROR = BAUD_RATE_ERROR / BAUD_RATE;
-
-
 localparam      STOP_BIT_CYCLES          = (BAUD_CYCLES_FRAC == 0) ? BAUD_CYCLES : (BAUD_CYCLES + 1);
 
+localparam [9:0] ADDITION_CYCLES = (BAUD_CYCLES_FRAC == 0) ? 10'b0000000000 :
+                                   (BAUD_CYCLES_FRAC == 1) ? 10'b0000010000 :
+                                   (BAUD_CYCLES_FRAC == 2) ? 10'b0010000100 :
+                                   (BAUD_CYCLES_FRAC == 3) ? 10'b0010010010 :
+                                   (BAUD_CYCLES_FRAC == 4) ? 10'b0101001010 :
+                                   (BAUD_CYCLES_FRAC == 5) ? 10'b0101010101 :
+                                   (BAUD_CYCLES_FRAC == 6) ? 10'b1010110101 :
+                                   (BAUD_CYCLES_FRAC == 7) ? 10'b1101101101 :
+                                   (BAUD_CYCLES_FRAC == 8) ? 10'b1101111011 :
+                                  /*BAUD_CYCLES_FRAC == 9)*/ 10'b1111101111 ;
 
 wire [31:0] cycles [9:0];
 
-generate if (BAUD_CYCLES_FRAC == 0) begin
-    assign cycles[0] = BAUD_CYCLES    ;
-    assign cycles[1] = BAUD_CYCLES    ;
-    assign cycles[2] = BAUD_CYCLES    ;
-    assign cycles[3] = BAUD_CYCLES    ;
-    assign cycles[4] = BAUD_CYCLES    ;
-    assign cycles[5] = BAUD_CYCLES    ;
-    assign cycles[6] = BAUD_CYCLES    ;
-    assign cycles[7] = BAUD_CYCLES    ;
-    assign cycles[8] = BAUD_CYCLES    ;
-    assign cycles[9] = BAUD_CYCLES    ;
-end else if (BAUD_CYCLES_FRAC == 1) begin
-    assign cycles[0] = BAUD_CYCLES    ;
-    assign cycles[1] = BAUD_CYCLES    ;
-    assign cycles[2] = BAUD_CYCLES    ;
-    assign cycles[3] = BAUD_CYCLES    ;
-    assign cycles[4] = BAUD_CYCLES + 1;
-    assign cycles[5] = BAUD_CYCLES    ;
-    assign cycles[6] = BAUD_CYCLES    ;
-    assign cycles[7] = BAUD_CYCLES    ;
-    assign cycles[8] = BAUD_CYCLES    ;
-    assign cycles[9] = BAUD_CYCLES    ;
-end else if (BAUD_CYCLES_FRAC == 2) begin
-    assign cycles[0] = BAUD_CYCLES    ;
-    assign cycles[1] = BAUD_CYCLES    ;
-    assign cycles[2] = BAUD_CYCLES + 1;
-    assign cycles[3] = BAUD_CYCLES    ;
-    assign cycles[4] = BAUD_CYCLES    ;
-    assign cycles[5] = BAUD_CYCLES    ;
-    assign cycles[6] = BAUD_CYCLES    ;
-    assign cycles[7] = BAUD_CYCLES + 1;
-    assign cycles[8] = BAUD_CYCLES    ;
-    assign cycles[9] = BAUD_CYCLES    ;
-end else if (BAUD_CYCLES_FRAC == 3) begin
-    assign cycles[0] = BAUD_CYCLES    ;
-    assign cycles[1] = BAUD_CYCLES + 1;
-    assign cycles[2] = BAUD_CYCLES    ;
-    assign cycles[3] = BAUD_CYCLES    ;
-    assign cycles[4] = BAUD_CYCLES + 1;
-    assign cycles[5] = BAUD_CYCLES    ;
-    assign cycles[6] = BAUD_CYCLES    ;
-    assign cycles[7] = BAUD_CYCLES + 1;
-    assign cycles[8] = BAUD_CYCLES    ;
-    assign cycles[9] = BAUD_CYCLES    ;
-end else if (BAUD_CYCLES_FRAC == 4) begin
-    assign cycles[0] = BAUD_CYCLES    ;
-    assign cycles[1] = BAUD_CYCLES + 1;
-    assign cycles[2] = BAUD_CYCLES    ;
-    assign cycles[3] = BAUD_CYCLES + 1;
-    assign cycles[4] = BAUD_CYCLES    ;
-    assign cycles[5] = BAUD_CYCLES    ;
-    assign cycles[6] = BAUD_CYCLES + 1;
-    assign cycles[7] = BAUD_CYCLES    ;
-    assign cycles[8] = BAUD_CYCLES + 1;
-    assign cycles[9] = BAUD_CYCLES    ;
-end else if (BAUD_CYCLES_FRAC == 5) begin
-    assign cycles[0] = BAUD_CYCLES + 1;
-    assign cycles[1] = BAUD_CYCLES    ;
-    assign cycles[2] = BAUD_CYCLES + 1;
-    assign cycles[3] = BAUD_CYCLES    ;
-    assign cycles[4] = BAUD_CYCLES + 1;
-    assign cycles[5] = BAUD_CYCLES    ;
-    assign cycles[6] = BAUD_CYCLES + 1;
-    assign cycles[7] = BAUD_CYCLES    ;
-    assign cycles[8] = BAUD_CYCLES + 1;
-    assign cycles[9] = BAUD_CYCLES    ;
-end else if (BAUD_CYCLES_FRAC == 6) begin
-    assign cycles[0] = BAUD_CYCLES + 1;
-    assign cycles[1] = BAUD_CYCLES    ;
-    assign cycles[2] = BAUD_CYCLES + 1;
-    assign cycles[3] = BAUD_CYCLES    ;
-    assign cycles[4] = BAUD_CYCLES + 1;
-    assign cycles[5] = BAUD_CYCLES + 1;
-    assign cycles[6] = BAUD_CYCLES    ;
-    assign cycles[7] = BAUD_CYCLES + 1;
-    assign cycles[8] = BAUD_CYCLES    ;
-    assign cycles[9] = BAUD_CYCLES + 1;
-end else if (BAUD_CYCLES_FRAC == 7) begin
-    assign cycles[0] = BAUD_CYCLES + 1;
-    assign cycles[1] = BAUD_CYCLES    ;
-    assign cycles[2] = BAUD_CYCLES + 1;
-    assign cycles[3] = BAUD_CYCLES + 1;
-    assign cycles[4] = BAUD_CYCLES    ;
-    assign cycles[5] = BAUD_CYCLES + 1;
-    assign cycles[6] = BAUD_CYCLES + 1;
-    assign cycles[7] = BAUD_CYCLES    ;
-    assign cycles[8] = BAUD_CYCLES + 1;
-    assign cycles[9] = BAUD_CYCLES + 1;
-end else if (BAUD_CYCLES_FRAC == 8) begin
-    assign cycles[0] = BAUD_CYCLES + 1;
-    assign cycles[1] = BAUD_CYCLES + 1;
-    assign cycles[2] = BAUD_CYCLES    ;
-    assign cycles[3] = BAUD_CYCLES + 1;
-    assign cycles[4] = BAUD_CYCLES + 1;
-    assign cycles[5] = BAUD_CYCLES + 1;
-    assign cycles[6] = BAUD_CYCLES + 1;
-    assign cycles[7] = BAUD_CYCLES    ;
-    assign cycles[8] = BAUD_CYCLES + 1;
-    assign cycles[9] = BAUD_CYCLES + 1;
-end else /*if (BAUD_CYCLES_FRAC == 9)*/ begin
-    assign cycles[0] = BAUD_CYCLES + 1;
-    assign cycles[1] = BAUD_CYCLES + 1;
-    assign cycles[2] = BAUD_CYCLES + 1;
-    assign cycles[3] = BAUD_CYCLES + 1;
-    assign cycles[4] = BAUD_CYCLES    ;
-    assign cycles[5] = BAUD_CYCLES + 1;
-    assign cycles[6] = BAUD_CYCLES + 1;
-    assign cycles[7] = BAUD_CYCLES + 1;
-    assign cycles[8] = BAUD_CYCLES + 1;
-    assign cycles[9] = BAUD_CYCLES + 1;
-end endgenerate
+assign cycles[0] = BAUD_CYCLES + (ADDITION_CYCLES[0] ? 1 : 0);
+assign cycles[1] = BAUD_CYCLES + (ADDITION_CYCLES[1] ? 1 : 0);
+assign cycles[2] = BAUD_CYCLES + (ADDITION_CYCLES[2] ? 1 : 0);
+assign cycles[3] = BAUD_CYCLES + (ADDITION_CYCLES[3] ? 1 : 0);
+assign cycles[4] = BAUD_CYCLES + (ADDITION_CYCLES[4] ? 1 : 0);
+assign cycles[5] = BAUD_CYCLES + (ADDITION_CYCLES[5] ? 1 : 0);
+assign cycles[6] = BAUD_CYCLES + (ADDITION_CYCLES[6] ? 1 : 0);
+assign cycles[7] = BAUD_CYCLES + (ADDITION_CYCLES[7] ? 1 : 0);
+assign cycles[8] = BAUD_CYCLES + (ADDITION_CYCLES[8] ? 1 : 0);
+assign cycles[9] = BAUD_CYCLES + (ADDITION_CYCLES[9] ? 1 : 0);
 
 
 
@@ -380,39 +279,62 @@ assign f_tready = state[0];   // (state == S_IDLE)
 // parameter checking
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
 initial begin
-    if (FIFO_EA > 0 && FIFO_EA < 3) begin
-        $error("*** error : uart_tx : FIFO_EA can not be 1 or 2");
-        $stop;
-    end
+    if (BYTE_WIDTH <= 0) begin $error("invalid parameter : BYTE_WIDTH<=0"); $stop; end
+    if (STOP_BITS  <= 0) begin $error("invalid parameter : STOP_BITS <=0"); $stop; end
+    if (BAUD_CYCLES < 1) begin $error("invalid parameter : BAUD_CYCLES < 1, please use a faster driving clock"); $stop; end
     
-    if (BYTE_WIDTH <= 0) begin
-        $error("*** error : uart_tx : invalid parameter : BYTE_WIDTH<=0");
-        $stop;
-    end
+    $display("uart_tx :           parity = %s" , PARITY );
+    $display("uart_tx :     clock period = %.0f ns   (%-10d Hz)" , 1000000000.0/CLK_FREQ  , CLK_FREQ );
+    $display("uart_tx : baud rate period = %.0f ns   (%-10d Hz)" , 1000000000.0/BAUD_RATE , BAUD_RATE);
+    $display("uart_tx :      baud cycles = %-10d"   , BAUD_CYCLES );
+    $display("uart_tx : baud cycles frac = %-10d"   , BAUD_CYCLES_FRAC  );
     
-    if (STOP_BITS  <= 0) begin
-        $error("*** error : uart_tx : invalid parameter : STOP_BITS <=0");
-        $stop;
-    end
-    
-    // print information
-    $display ("uart_tx :                  clock frequency = %10d Hz" , CLK_FREQ                 );
-    $display ("uart_tx :                desired baud rate = %10d Hz" , BAUD_RATE                );
-    $display ("uart_tx :  ideal frequency division factor = %.6f"    , IDEAL_BAUD_CYCLES        );
-    $display ("uart_tx : actual frequency division factor = %.6f"    , ACTUAL_BAUD_CYCLES       );
-    $display ("uart_tx :                 actual baud rate = %.3f Hz" , ACTUAL_BAUD_RATE         );
-    $display ("uart_tx :      relative error of baud rate = %.6f%%"  , BAUD_RATE_RELATIVE_ERROR*100 );
-    
-    if (BAUD_CYCLES < 1) begin
-        $error("*** error : uart_tx : invalid parameter : BAUD_CYCLES < 1, please use a faster driving clock");
-        $stop;
-    end
-    
-    if ( BAUD_RATE_RELATIVE_ERROR > 0.005 ) begin
-        $error("*** error : uart_tx : relative error of baud rate is too large, please use faster driving clock, or integer multiple of baud rate.");
-        $stop;
+    if (PARITY == "ODD" || PARITY == "EVEN") begin
+        $display("uart_tx :             __      ____ ____ ____ ____ ____ ____ ____ ____________ ");
+        $display("uart_tx :        wave   \\____/____X____X____X____X____X____X____X____X____/   ");
+        $display("uart_tx :        bits   | S  | B0 | B1 | B2 | B3 | B4 | B5 | B6 | B7 | P  |   ");
+        $display("uart_tx : time_points  t0   t1   t2   t3   t4   t5   t6   t7   t8   t9   t10  ");
+        $display("uart_tx :");
+    end else begin
+        $display("uart_tx :             __      ____ ____ ____ ____ ____ ____ ____ _______ ");
+        $display("uart_tx :        wave   \\____/____X____X____X____X____X____X____X____/   ");
+        $display("uart_tx :        bits   | S  | B0 | B1 | B2 | B3 | B4 | B5 | B6 | B7 |   ");
+        $display("uart_tx : time_points  t0   t1   t2   t3   t4   t5   t6   t7   t8   t9   ");
+        $display("uart_tx :");
     end
 end
+
+generate genvar index, i;
+    for (index=0; index<=9; index=index+1) begin : print_and_check_time
+        localparam cycles_acc = ( (index >= 0) ? (BAUD_CYCLES + (ADDITION_CYCLES[0] ? 1 : 0)) : 0 )
+                              + ( (index >= 1) ? (BAUD_CYCLES + (ADDITION_CYCLES[1] ? 1 : 0)) : 0 )
+                              + ( (index >= 2) ? (BAUD_CYCLES + (ADDITION_CYCLES[2] ? 1 : 0)) : 0 )
+                              + ( (index >= 3) ? (BAUD_CYCLES + (ADDITION_CYCLES[3] ? 1 : 0)) : 0 )
+                              + ( (index >= 4) ? (BAUD_CYCLES + (ADDITION_CYCLES[4] ? 1 : 0)) : 0 )
+                              + ( (index >= 5) ? (BAUD_CYCLES + (ADDITION_CYCLES[5] ? 1 : 0)) : 0 )
+                              + ( (index >= 6) ? (BAUD_CYCLES + (ADDITION_CYCLES[6] ? 1 : 0)) : 0 )
+                              + ( (index >= 7) ? (BAUD_CYCLES + (ADDITION_CYCLES[7] ? 1 : 0)) : 0 )
+                              + ( (index >= 8) ? (BAUD_CYCLES + (ADDITION_CYCLES[8] ? 1 : 0)) : 0 )
+                              + ( (index >= 9) ? (BAUD_CYCLES + (ADDITION_CYCLES[9] ? 1 : 0)) : 0 ) ;
+        
+        localparam real ideal_time_ns  = ((index+1)*1000000000.0/BAUD_RATE);
+        localparam real actual_time_ns = (cycles_acc*1000000000.0/CLK_FREQ);
+        localparam real error          = (ideal_time_ns>actual_time_ns) ? (ideal_time_ns-actual_time_ns) : (-ideal_time_ns+actual_time_ns);
+        localparam real relative_error_percent = (error / (1000000000.0/BAUD_RATE)) * 100.0;
+        
+        initial if (PARITY == "ODD" || PARITY == "EVEN" || index < 9) begin
+            $display("uart_tx : t%-2d- t0 = %.0f ns (ideal)  %.0f ns (actual).   error=%.0f ns   relative_error=%.3f%%" ,
+                (index+1) ,
+                ideal_time_ns ,
+                actual_time_ns,
+                error,
+                relative_error_percent
+            );
+            
+            if ( relative_error_percent > 3.0 ) begin $error("relative_error is too large"); $stop; end   // if relative error larger than 3%
+        end
+    end
+endgenerate
 
 
 endmodule
